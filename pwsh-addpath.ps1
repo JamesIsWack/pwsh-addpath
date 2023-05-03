4728fb9b4c1bd3a386f4e5c9ac53be6828c3535a
kernaltrap <94473358+JamesIsWack@users.noreply.github.com>
2023-05-03 09:34:17 -0500

    Update pwsh-addpath.ps1

--- pwsh-addpath.ps1
+++ pwsh-addpath.ps1
@@ -3,7 +3,7 @@
 # Version 0.1
 # Run "Set-ExecutionPolicy Unrestricted -Scope CurrentUser" if security policy doesnt allow unsigned scripts. Reset to default afterwards.
 
-$addPath = 'C:\git-2.40.1;C:\kdenlive\bin;C:\krita-x64-5.1.5-setup\bin;C:\node-v18.15.0-win-x64;C:\OBS-Studio;C:\Python311;C:\SysGCC\powerpc-eabi\bin;C:\VSCode-win32-x64-1.74.3'
+$addPath = '' # Paths go here, seperate with semicolons.
 
 Function Add-PathVariable {
     param (
